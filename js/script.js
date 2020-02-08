caed8fa0226640d44ad4e9fc02d065116547b610
Rekhansh <rekhansh.singhwal@gmail.com>
2020-02-08 23:38:16 +0530

    Updated
    Updated

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("experience-details").hidden = true
+document.getElementById("experience-simple").onclick= function()
+{
+    document.getElementById("experience-details").hidden = !document.getElementById("experience-details").hidden;
+}
+document.getElementById("nav-icon").onclick= function()
+{
+    document.getElementById("nav").hidden = !document.getElementById("nav").hidden;
+}
